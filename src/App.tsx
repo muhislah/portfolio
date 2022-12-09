0a8432e845a98d157ff61191cdb9c4aca2a8ea82
muhislah <el.islah.udin@gmail.com>
2022-12-10 00:28:59 +0700

    add tailwind and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../public/vite.svg'
-import './App.css'
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NotFound from './pages/404'
+import Dashboard from './pages/Dashboard'
+import Message from './pages/Message'
+import Profile from './pages/Profile'
 
+const router = createBrowserRouter([
+	{ path: '/portfolio', element: <Dashboard /> },
+	{ path: '/portfolio/profile', element: <Profile /> },
+	{ path: '/portfolio/message', element: <Message /> },
+	{ path: '*', element: <NotFound />}
 
-function App() {
-  const [count, setCount] = useState(0)
+])
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const App = () => {
+	return (
+		<RouterProvider router={router} />
+	)
 }
 
 export default App
